36ddc3e353de8e012b47b167521f6a6ca182935b
Oleksii Vovnenko <kespa71for@gmail.com>
2024-02-20 20:13:43 +0100

    refactor code

--- src/store/reducers/ActionCrators.ts
+++ src/store/reducers/ActionCrators.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import { IPost } from "../../models";
+import {IPost} from "../../models";
 
 export const fetchPosts = createAsyncThunk(
- "posts/fetchAll",
- async (_, thunkAPI) => {
-     try {
-      const response = await axios.get<IPost[]>(`https://jsonplaceholder.typicode.com/posts?_page=1&_limit=10`)
-      return response.data
-     } catch (e) {
-      return thunkAPI.fulfillWithValue("Error")
-     }
- }
+    "posts/fetchAll",
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get<IPost[]>(`https://jsonplaceholder.typicode.com/posts?_page=1&_limit=10`)
+            return response.data
+        } catch (e) {
+            return thunkAPI.fulfillWithValue("Error")
+        }
+    }
 )
