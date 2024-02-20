d06624fe9dab59494f0c6029a403c6ff27eeee34
Oleksii Vovnenko <kespa71for@gmail.com>
2024-02-20 20:13:23 +0100

    add extra reducer: for fetch post

--- src/store/reducers/PostsSlice.ts
+++ src/store/reducers/PostsSlice.ts
@@ -5,19 +5,41 @@ import {fetchPosts} from "./ActionCrators";
 interface IPostsState {
     isLoading: boolean,
     posts: IPost[],
-    error: string
+    error: string | null
 }
 
 const initialState: IPostsState = {
     isLoading: false,
     posts: [],
-    error: ""
+    error: null
 };
 
 export const postsSlice = createSlice({
     name: "posts",
     initialState,
     reducers: {},
+    extraReducers: ({addCase}) => {
+        const { pending, rejected, fulfilled } = fetchPosts;
+
+        addCase(pending,
+            (state) => {
+            state.isLoading = true;
+            state.error = null;
+        });
+
+        // @ts-ignore
+        addCase(fulfilled,
+            (state, action: PayloadAction<IPost[]>) => {
+            state.isLoading = false;
+            state.posts = action.payload;
+        });
+
+        addCase(rejected,
+            (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message || "Something went wrong";
+        });
+    }
 });
 
 export default postsSlice.reducer;
