d4f7924da4f536c873435c5caf0c2b4c2eb761b0
Oleksii Vovnenko <kespa71for@gmail.com>
2024-02-21 20:39:06 +0100

    add component in App: postContainer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import Header from './components/Header/Header';
 import Posts from "./pages/Posts/Posts";
+import PostContainer from "./components/PostContainer/PostContainer";
 
 function App() {
-  return (
-    <>
-      <Header/>
-      <Posts/>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Posts/>
+            <PostContainer/>
+        </>
+    );
 }
 
 export default App;
