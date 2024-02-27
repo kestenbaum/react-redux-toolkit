24fe9460d8f66de76248db4645d1cde3fe5f5c8a
Oleksii Vovnenko <kespa71for@gmail.com>
2024-02-27 22:26:58 +0100

    create router

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import {
+    Posts,
+    Users
+} from "../pages";
+
+export const routes = [
+    {
+        path: "/",
+        component: Posts
+    },
+    {
+        path: "/users",
+        component: Users
+    }
+]
