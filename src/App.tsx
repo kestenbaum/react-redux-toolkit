c6ae956f130247e2271732e1faaa490ee649d91b
Oleksii Vovnenko <kespa71for@gmail.com>
2024-02-27 21:27:17 +0100

    delete component PostContainer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Header from './components/Header/Header';
 import Posts from "./pages/Posts/Posts";
-import PostContainer from "./components/PostContainer/PostContainer";
 
 function App() {
     return (
         <>
             <Header/>
             <Posts/>
-            <PostContainer/>
         </>
     );
 }
