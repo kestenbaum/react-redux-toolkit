0543251c23e8f632973b2b6e4d1f647c110dfbb7
Oleksii Vovnenko <kespa71for@gmail.com>
2024-02-27 21:26:25 +0100

    add component HeaderNavigate

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React, {FC} from 'react';
 
 import style from "./Header.module.css";
+import HeaderNavigate from "./HeaderNavigate/HeaderNavigate";
 const Header:FC = () => {
     return (
      <header className={style.header}>
         <div className="container">
             <div className={style.wrapper}>
                 Logo
+                <HeaderNavigate/>
             </div>
         </div>
      </header>
