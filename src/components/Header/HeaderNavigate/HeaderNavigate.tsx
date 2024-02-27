3fffb93f21c99265f08c38c3bcae265a344b9ccd
Oleksii Vovnenko <kespa71for@gmail.com>
2024-02-27 22:27:47 +0100

    update component HeaderNavigate: add Link

--- src/components/Header/HeaderNavigate/HeaderNavigate.tsx
+++ src/components/Header/HeaderNavigate/HeaderNavigate.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import style from "../HeaderNavigate/HeaderNavigate.module.css";
 
 const HeaderNavigate = () => {
     return (
         <nav className={style.nav}>
             <ul className={style.list}>
-                <li><a href="#" className={style.link}>Posts</a></li>
-                <li><a href="#" className={style.link}>Users</a></li>
+                <li><Link to="/" className={style.link}>Posts</Link></li>
+                <li><Link to="/users" className={style.link}>Users</Link></li>
             </ul>
         </nav>
     );
